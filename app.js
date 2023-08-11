fa91a163c1922b8d729da43c51e6ce5006ffd512
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-08-11 09:30:01 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const question =e.currentTarget.parentElement;
+
+        console.log(question);
+    question.classList.toggle("show-text");
+  });
+});
